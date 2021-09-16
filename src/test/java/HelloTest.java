e1fe4c1835ca98cb307a68e882724655de405530
sayhan01 <sayhan042@gmail.com>
2021-09-16 18:27:58 +0200

    second update

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,5 +1,7 @@
 public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello test");
+
+        System.out.println("About sharing project about on GİTHUB");
     }
 }
